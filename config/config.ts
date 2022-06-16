adccd805fba96dd1d1e55303d840ee27b3bc51f8
xydinga <>
2022-06-16 23:54:21 +0800

    init

---
+++ config/config.ts
@@ -0,0 +1,13 @@
+export default {
+  title: 'react-hooks',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  menus: {
+    '/': [
+      {
+        title: 'Home',
+        path: 'index',
+      },
+    ],
+  },
+};
